56b87be67e171fbd718761a82e28d2dc6b2d64dc
= <=>
2020-07-12 20:24:31 +0700

    Add some change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Screen Cast
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <h1 style="color: red;">Laravel Screen Cast</h1>
                 </div>
             </div>
         </div>
